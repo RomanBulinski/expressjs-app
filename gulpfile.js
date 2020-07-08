2d4948b947adc6bb517ae5021b9d7f15ea8728ba
roman <aboandrus@gmail.com>
2020-07-08 19:35:44 +0200

    dodano gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+
+gulp.task('buildjs', async function(){
+    gulp.src('source/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
